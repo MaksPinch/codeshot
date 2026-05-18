4841cc08404fccdf6e30b4a47ef9b9b3b755a8c3
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2026-05-18 17:28:26 +0200

    changed view.py using correct from implementation

--- codeshot/views.py
+++ codeshot/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+
+from .forms import CodeInputForm 
+
+
 def home_view(request):
+    if request.method == "POST":
+        form = CodeInputForm(request.POST)
+        if form.is_valid():
+            preview_data = form.cleaned_data
+        else:
+            preview_data = None
+    else:
+        form = CodeInputForm()
+        preview_data = None
+
     context = {
         "title": "CodeShot",
         "subtitle": "Create syntax-highlighted code previews.",
+        "form": form,
+        "preview_data": preview_data,
     }
-    return render(request, 'codeshot/home.html', context)
 
+    return render(request, "codeshot/home.html", context)
