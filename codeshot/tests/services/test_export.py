f270402a9f4d7988f42e76d6b52507952a449669
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2026-06-17 21:37:35 +0200

    test(services): add unit tests for generate_image function

---
+++ codeshot/tests/services/test_export.py
@@ -0,0 +1,31 @@
+import pytest
+
+from codeshot.services.exports import generate_image
+
+RANDDOME_SETTINGS = {
+    "code": "print('export')",
+    "language": "python",
+    "filename": "export.py",
+    "theme": "default",
+    "font_size": 14,
+    "padding": 24,
+}
+
+
+def test_generate_export_return_png_bytes():
+    result = generate_image(RANDDOME_SETTINGS, "png")
+
+    assert isinstance(result, bytes)
+    assert result.startswith(b"\x89PNG")
+
+
+def test_generate_export_return_jpg_bytes():
+    result = generate_image(RANDDOME_SETTINGS, "jpg")
+
+    assert isinstance(result, bytes)
+    assert result.startswith(b"\xff\xd8")
+
+
+def test_generate_export_return_unknownformat():
+    with pytest.raises(ValueError):
+        result = generate_image(RANDDOME_SETTINGS, "txt")
