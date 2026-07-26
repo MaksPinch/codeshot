029b53e2b0c70518d9380dfc4ef94cac3a402054
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2026-07-26 15:51:30 +0200

    feat: create serialize_user in codeshot/services/auth.py

--- codeshot/services/auth.py
+++ codeshot/services/auth.py
@@ -6,3 +6,12 @@ def auth_user(cleaned_data):
     return User.objects.create_user(
         username=cleaned_data["username"], password=cleaned_data["password"]
     )
+
+
+def serialize_user(user):
+    return {
+        "id": user.id,
+        "username": user.username,
+        "groups": list(user.groups.values_list("name", flat=True)),
+        "permissions": list(user.get_all_permissions()),
+    }
