52c2bc417a3d86df714575fbfd6c33900f21ab42
Maksim Pinchuk <pinchukmaksim013@gmail.com>
2026-07-28 15:00:59 +0200

    feat: implement management command setup_groups

---
+++ codeshot/management/commands/setup_groups.py
@@ -0,0 +1,24 @@
+# Ваше_приложение/management/commands/setup_groups.py
+from django.contrib.auth.models import Group, Permission
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        group_export_users, created_export_users = Group.objects.get_or_create(
+            name="Export users"
+        )
+        group_analysts_users, created_analysts_users = Group.objects.get_or_create(
+            name="Analysts"
+        )
+
+        export_perm = Permission.objects.get(
+            content_type__app_label="codeshot", codename="export_images"
+        )
+
+        stats_perm = Permission.objects.get(
+            content_type__app_label="codeshot", codename="view_product_stats"
+        )
+        
+        group_export_users.permissions.add(export_perm)
+        group_analysts_users.permissions.add(stats_perm)
